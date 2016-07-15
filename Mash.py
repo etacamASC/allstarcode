7c88584a50635bc8959a28659d31916312cc8e59
etacamASC <etacam3554@bths.edu>
2016-07-15 15:22:20 -0400

    ASC
    7/15/2016

---
+++ Mash.py
@@ -0,0 +1,28 @@
+from random import *
+
+print ("Let's play MASH")
+
+myWives=["Justin Bieber","Miley Cyrus","Kim Kardasian"]
+myWives.append(raw_input("Who do you want to marry?"))
+
+
+myPets=["Donkey","Cow","Rat"]
+myPets.append(raw_input("What type of pet do you want?"))
+
+myRec=["Striper","Artist","Terrorist"]
+myRec.append(raw_input("What do you want to be known as?"))
+
+myCars=["Mercedes-Benz Maybach Exelero","Lexus RX 350", "Ford Escape"]
+myCars.append(raw_input("What type of car do you want?"))
+
+print ("You'll marry...")
+print (choice(myWives))
+
+print ("Your pet will be...")
+print (choice(myPets))
+
+print ("You will be known as a...")
+print (choice(myRec))
+
+print ("Your car will be a...")
+print (choice(myCars))
