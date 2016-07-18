77e82fade018881850fb41863bc321d0cfb150c5
etacamASC <etacam3554@bths.edu>
2016-07-18 10:39:51 -0400

    ASC
    7/18/2016

---
+++ Obamafication.py
@@ -0,0 +1,17 @@
+from Myro import *
+ObamaDarkBlue = makeColor(0,51,76)
+ObamaRed = makeColor(217, 26, 33)
+ObamaBlue = makeColor(112,150,158)
+ObamaYellow = makeColor(252, 227, 166)
+pic=makePicture(pickAFile())
+for pixel in getPixels(pic):
+    gray=getGray(pixel)
+    if gray>180:
+        setColor(pixel,ObamaYellow)
+    elif gray>120:
+        setColor(pixel,ObamaBlue)
+    elif gray>60:
+        setColor(pixel,ObamaRed)
+    else:
+        setColor(pixel,ObamaDarkBlue)
+show(pic)
