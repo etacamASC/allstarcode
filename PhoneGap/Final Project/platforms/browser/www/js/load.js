32c8b4227746d8c5c9fa17c60f0de521ba272f32
etacamASC <etacam3554@bths.edu>
2016-08-12 11:30:55 -0400

    ASC
    8/12/2016

--- PhoneGap/Final Project/platforms/browser/www/js/load.js
+++ PhoneGap/Final Project/platforms/browser/www/js/load.js
@@ -1,17 +1,3 @@
-//this list will hold the information for the goal, it starts empty but ends up completely filled
-//as the app goes on. We will be able to get this infromation from a file, delet it change it 
-var goalData = [
-				null /*NAME*/, null /*START_DAY(mm/dd/yyyy)*/, 
-				null /*END_DAY(mm/dd/yyyy)*/, null /*DESCRIPTION*/,
-				null /*STEPS-a list of smart goals*/
-				];
-/*
-Name
-time span: (mm/dd/yyyy) - (mm/dd/yyyy)
-bunch of text - (Decription)
-list of step
-*/
-
 //----------------------getters, setters, and checker of the data-------------------------------//
 function getName(list){
 	return list[0];
@@ -47,6 +33,8 @@ function getSteps(list){
 function getStep(list,index){
 	return list[4][index];
 }
+
+
 /*This data for this function many need to be specail made*/
 function giveSteps(list,input){}
 function giveStep(list,index,input){}
@@ -58,6 +46,17 @@ function checkListInListVal(list,indexList1, indexList2, excpectedValue){
 	return (list[indexList1][indexList2] == excpectedValue);
 }
 //---------------------end of the getters, setters, and checkers group-------------------------//
+
+// A map of what a goal should look like
+/*
+var goalData = [null NAME, null START_DAY(mm/dd/yyyy), null END_DAY(mm/dd/yyyy), null DESCRIPTION, null STEPS-a list of smart goals];
+*/
+/*
+Name
+time span: (mm/dd/yyyy) - (mm/dd/yyyy)
+bunch of text - (Decription)
+list of step
+*/
 
 var exampleGoal1 = ["Back to School Goal", "8/10/2016",
 					"8/12/2016", "This is a goal I'm setting to help me complete my goals for the school.",
@@ -76,33 +75,66 @@ var exampleGoal4 = ["Love Your Mom", "8/10/2016",
 					["Smart1","Smart2","Smart3","Smart4"]];
 
 var goals = [exampleGoal1,exampleGoal2,exampleGoal3,exampleGoal4];
+
+
 /*FormateToHTML()
+
 this data returned by this method is ment to be used as the first
 argument in the pushToMainPage().
+
 it get the list and turns it to HTML and returns it as a string.
 */
-
 function FormateToHTML(goal) {
 	var name = getName(goal);
 	var start = getStart(goal);
 	var end = getEnd(goal);
 	var decription = getDescription(goal);
 	/*var smart = will become the variable that get SMART goal once I figure out how to get it*/
-	return ('<div class= "goal"> <h1 class ="goal_name">'+name+'</h1><p class = "date">'+start+'-'+end+'</p><p class="decription">'+decription+'</p></div>');
+	return ('<div class="content-top-grids-box"><div class="content-top-grid"></div><div class="content-top-grid-title"><div class="content-top-grid-title-left"><h4><a href="iconsingle.html">'+name+'</a></h4><ul><li><a href="iconsingle.html">'+start+'-'+end+'</a></li><li><a href="iconsingle.html">Goal</a></li></ul></div><div class="content-top-grid-title-right"><a href="iconsingle.html"></a></div><div class="clear"> </div></div></div>');
 }
 
 //pushes the code formated to HTML to the index.html
 //with idOrClasses true == id and false == class
-function pushToMainPage(HTML_Text, look_For_By, idOrClass){
+function pushToMainPage(HTML_Text, look_For, idOrClass){
 	if (idOrClass) {
-		document.getElementById(look_For_By).innerHTML = document.getElementById(look_For_By).innerHTML + HTML_Text ;
+		document.getElementById(look_For).innerHTML = document.getElementById(look_For).innerHTML + HTML_Text ;
 	} else {
-		document.getElementsByClassName(look_For_By).innerHTML = document.getElementById(look_For_By).innerHTML + HTML_Text;
+		document.getElementsByClassName(look_For).innerHTML = document.getElementById(look_For).innerHTML + HTML_Text;
+	}
+}
+
+function resetHMTL(look_For, idOrClass){
+	if (idOrClass) {
+		document.getElementById(look_For).innerHTML = '';
+	} else {
+		document.getElementsByClassName(look_For).innerHTML = '';
 	}
 }
 
 function pushAllLoadedGoalsToMainPage(list_Of_Goals, put_In, idOrClass){
+	//resets the HTML in the element
+	resetHMTL(put_In,idOrClass);
+
+	/* 
+	This block of code puts the header on the list everytime goals are 
+	loaded to the main page.
+	*/
+	var header = '<div class="content-top-grids"><div class="content-top-grids" ><div class="wrap"><div class="content-top-header"><h3>Goals</h3><p>Organize and complete your task!</p></div><div class="clear"> </div>';
+	pushToMainPage(header,put_In,idOrClass);
+
+	/*
+	This for loop add the goals
+	*/
 	for (var i = 0; i <= list_Of_Goals.length-1; i++) {
 	 	pushToMainPage(FormateToHTML(list_Of_Goals[i]), put_In, idOrClass);
-	 } 
+	 }
+
+	 /*
+	This block of code puts the ender on the list everytime goals are 
+	loaded to the main page. 
+	 */
+	 var ender = '<div class="clear"> </div><a class="btn" href="iconsingle.html">More Goals</a></div></div>';
+	 pushToMainPage(ender,put_In,idOrClass);
 }
+
+loadpage
